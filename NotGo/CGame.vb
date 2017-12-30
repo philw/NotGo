a8e7f7e0aee110aa5ffb9f94c39899bd56888d4b
Phil Wood <phil@pwood.net>
2017-12-30 11:02:21 +0000

    Just started

---
+++ NotGo/CGame.vb
@@ -0,0 +1,23 @@
+﻿Public Class CGame
+
+    Private Const BoardSize As Integer = 9
+    Private Board(BoardSize - 1, BoardSize - 1) As Char
+
+
+
+    Public Sub New()
+
+        ' initialize the board to all spaces (empty)
+        For y = 0 To BoardSize - 1
+            For x = 0 To BoardSize - 1
+                Board(x, y) = " "
+            Next
+        Next
+
+
+    End Sub
+
+
+
+
+End Class
---
+++ NotGo/MainForm.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MoveLabel = New System.Windows.Forms.Label()
+        Me.BoardDisplay = New System.Windows.Forms.PictureBox()
+        CType(Me.BoardDisplay, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MoveLabel
+        '
+        Me.MoveLabel.AutoSize = True
+        Me.MoveLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MoveLabel.Location = New System.Drawing.Point(51, 9)
+        Me.MoveLabel.Name = "MoveLabel"
+        Me.MoveLabel.Size = New System.Drawing.Size(325, 55)
+        Me.MoveLabel.TabIndex = 0
+        Me.MoveLabel.Text = "Black to move"
+        '
+        'BoardDisplay
+        '
+        Me.BoardDisplay.BackColor = System.Drawing.Color.White
+        Me.BoardDisplay.Location = New System.Drawing.Point(61, 78)
+        Me.BoardDisplay.Name = "BoardDisplay"
+        Me.BoardDisplay.Size = New System.Drawing.Size(600, 600)
+        Me.BoardDisplay.TabIndex = 1
+        Me.BoardDisplay.TabStop = False
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(851, 699)
+        Me.Controls.Add(Me.BoardDisplay)
+        Me.Controls.Add(Me.MoveLabel)
+        Me.Name = "MainForm"
+        Me.Text = "Not Go"
+        CType(Me.BoardDisplay, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MoveLabel As Label
+    Friend WithEvents BoardDisplay As PictureBox
+End Class
